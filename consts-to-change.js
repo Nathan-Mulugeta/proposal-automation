1bfbdb7578034feb3b5ffeb2bd8417cb85215732
Nathan Mulugeta <nathanmulugetacoding@gmail.com>
2023-10-12 15:49:28 +0300

    first commit

---
+++ consts-to-change.js
@@ -0,0 +1,42 @@
+// Constants to change.
+const testing = true;
+
+const REPO_OWNER = testing ? "Nathan-Mulugeta" : "Expensify";
+
+const REPO_NAME = REPO_OWNER === "Expensify" ? "App" : "feedback-app";
+
+const SLACK_URL =
+  "https://expensify.slack.com/archives/C049HHMV9SM/p1697005474290679";
+
+// Comment body
+const COMMENT_BODY = `
+## Proposal
+
+### Please re-state the problem that we are trying to solve in this issue.
+There is an absence of an arrow icon in both the date and merchant fields.
+
+### What is the root cause of that problem?
+The root cause of this problem is here on these two lines:
+https://github.com/Expensify/App/blob/07b99243ad9a2fe09e857038c80e1c5d6a426d6d/src/components/MoneyRequestConfirmationList.js#L554
+
+and 
+https://github.com/Expensify/App/blob/07b99243ad9a2fe09e857038c80e1c5d6a426d6d/src/components/MoneyRequestConfirmationList.js#L576
+
+We are checking if it is a request type before rendering the right arrow. 
+
+### What changes do you think we should make in order to solve the problem?
+We can remove the check for the request type by changing this line of code: 
+\`\`\`javascript
+shouldShowRightIcon={!props.isReadOnly && isTypeRequest} 
+\`\`\`
+
+To:
+\`\`\`javascript
+shouldShowRightIcon={!props.isReadOnly} 
+\`\`\`
+
+### What alternative solutions did you explore? (Optional)
+NA
+`;
+
+export { COMMENT_BODY, REPO_NAME, REPO_OWNER, SLACK_URL };
---
+++ github-interaction.js
@@ -0,0 +1,74 @@
+import axios from "axios";
+import {
+  COMMENT_BODY,
+  REPO_NAME,
+  REPO_OWNER,
+  SLACK_URL,
+} from "./consts-to-change.js";
+
+const baseUrl = "https://api.github.com";
+
+const accessToken = "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";
+
+let commentPosted = false;
+
+console.log(REPO_OWNER, "/", REPO_NAME);
+
+async function main() {
+  try {
+    console.log("running main");
+    if (commentPosted) {
+      console.log("Comment already posted. Stopping further comments.");
+      process.exit(0);
+      return;
+    }
+
+    // Fetch issues
+    const issuesUrl = `${baseUrl}/repos/${REPO_OWNER}/${REPO_NAME}/issues`;
+    const headers = {
+      Authorization: `token ${accessToken}`,
+    };
+
+    const response = await axios.get(issuesUrl, { headers });
+
+    if (response.status === 200) {
+      const issues = response.data;
+
+      // Check if an issue contains the Slack URL
+      for (const issue of issues) {
+        if (issue.body.includes(SLACK_URL)) {
+          // Check if you already posted a comment on this issue
+          const commentsUrl = `${baseUrl}/repos/${REPO_OWNER}/${REPO_NAME}/issues/${issue.number}/comments`;
+          const commentsResponse = await axios.get(commentsUrl, { headers });
+          const comments = commentsResponse.data;
+
+          const alreadyCommented = comments.some(
+            (comment) => comment.body === COMMENT_BODY
+          );
+
+          console.log("No issue with the slack URL posted yet.");
+
+          if (alreadyCommented) {
+            console.log("Comment already posted. Stopping further comments.");
+            process.exit(0);
+          } else {
+            // Post the comment
+            await axios.post(commentsUrl, { body: COMMENT_BODY }, { headers });
+            console.log("Comment posted successfully.");
+            commentPosted = true;
+            process.exit(0);
+          }
+          return;
+        }
+      }
+    } else {
+      console.log(`Failed to fetch issues. Status code: ${response.status}`);
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+// Schedule the script to run every second
+const intervalId = setInterval(main, 2000);
+if (commentPosted) clearInterval(intervalId);
